3a50cbedb976eb53f6059044772ab387e1f60b7d
bcyvxhfucg-cmd <bcyvxhfucg@gmail.com>
2025-11-13 09:25:02 +0300

    Update pair.js

--- commands/pair.js
+++ commands/pair.js
@@ -1,1 +1,4 @@
-
+module.exports = async (sock, msg, args) => {
+  await sock.sendMessage(msg.key.remoteJid, { text: "⌛ جاري توليد رمز الاقتران..." });
+  // رمز الاقتران يُولّد داخل index.js عند الاتصال
+};
